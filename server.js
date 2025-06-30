736382e04ebd1295cf5906b19d74434fe1ee72a0
Flamescode <mutukukennedy34@gmail.com>
2025-06-30 07:23:08 +0300

    port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/github-webhook/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
